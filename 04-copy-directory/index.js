ebfcffc7cba423784bbf34d5efa645220294eb37
0101oak <115372729+0101oak@users.noreply.github.com>
2023-05-08 20:13:59 +0300

    feat: add copy of directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fsPromise = require('fs/promises');
+const path = require('path');
+const FILES = path.resolve(__dirname, 'files')
+const COPY = path.resolve(__dirname, 'files-copy');
 
+
+function copyDir() {
+  fsPromise.rm(COPY, { recursive: true, force: true })
+   .then( () => {
+     fsPromise.mkdir(COPY, { recursive: true });
+  })
+  .then( () => {
+    return fsPromise.readdir(FILES, { withFileTypes: true });
+  })
+  .then ( (files) => {
+    files.forEach(file => {
+      if (file.isFile()) {
+        const copy = path.resolve(FILES, file.name);
+        const copied = path.resolve(COPY, file.name);
+        fsPromise.copyFile(copy, copied);
+      }
+    });
+  });
+}
+
+copyDir()
